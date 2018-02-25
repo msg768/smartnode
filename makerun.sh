cb45b772c237a64d5951793c83fe93ca6c01c8f6
Markus GH <markus.ghambari@gmail.com>
2018-02-25 17:48:27 +1100

    Update makerun.sh

--- makerun.sh
+++ makerun.sh
@@ -4,16 +4,12 @@
 # Add the following to the crontab (i.e. crontab -e)
 # */5 * * * * ~/smartnode/makerun.sh
 
-if ps -A | grep smartcashd > /dev/null
+if ! ps -A | grep smartcashd > /dev/null
 then
-  exit
-else
   smartcashd
 fi
 
-if ps -A | grep cpulimit > /dev/null
+if ! ps -A | grep cpulimit > /dev/null
 then
-  exit
-else
   cpulimit -P /usr/bin/smartcashd -l 20
 fi
